625fac654310486966f339721be12fa329c0a147
alexeyrz-78 <alexeyrz@gmail.com>
2026-05-21 22:03:58 +0200

    Update Device.kt

--- app/src/main/java/com/example/huaweiblocker/Device.kt
+++ app/src/main/java/com/example/huaweiblocker/Device.kt
@@ -1,1 +1,9 @@
+package com.example.huaweiblocker
 
+// Простая модель данных для одного подключенного устройства
+data class Device(
+    val name: String,
+    val mac: String,
+    var isBlocked: Boolean,
+    val is5GHz: Boolean // Чтобы разделять девайсы по вкладкам частот
+)
